9bdd8ed79afe24a6b627dffd8d1347dc3a0ca185
nico-mania <55563178+nico-mania@users.noreply.github.com>
2022-04-26 14:45:46 +0200

    added new texture added slider for rotation speed

--- Assets/Scripts/SphereRotate.cs
+++ Assets/Scripts/SphereRotate.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SphereRotate : MonoBehaviour
 {
     [SerializeField] private GameObject cube;
-    [SerializeField] private float rotateSpeed = 100;
+    [SerializeField] private Slider Speed;
+    [SerializeField] private float rotateSpeed;
     
     // Start is called before the first frame update
     void Start()
@@ -17,5 +19,10 @@ public class SphereRotate : MonoBehaviour
     void Update()
     {
         transform.RotateAround(cube.transform.position, Vector3.back, rotateSpeed * Time.deltaTime);
+    }
+
+    public void OnValueChanged(float value)
+    {
+        rotateSpeed = value;
     }
 }
